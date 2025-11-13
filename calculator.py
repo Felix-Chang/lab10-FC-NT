980fb5e9ea3813209206e71455405ba6df1e814d
Felix Chang <felixchang@Felixs-MacBook-Air.local>
2025-11-13 13:10:12 -0500

    renamed subtract and logarithm

--- calculator.py
+++ calculator.py
@@ -25,7 +25,7 @@ def hypotenuse(a, b):
 
 def add(a, b): return a + b
 
-def sub(a, b): return a - b
+def subtract(a, b): return a - b
 
 def mul(a, b): return a * b
 
@@ -34,7 +34,7 @@ def div(a, b): # raise ZeroDivisionError if a == 0
         raise ZeroDivisionError
     return b / a 
 
-def log(a, b): # use math library + raise ValueError
+def logarithm(a, b): # use math library + raise ValueError
     if a < 0 or b < 0 or a == 1:
         raise ValueError
     return math.log(b, a) 
